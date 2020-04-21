fe7a2e227208e3a9a955125ba12c06ac0919aa0e
Mauricio Prado <mauricioprado00@gmail.com>
2020-04-21 11:45:36 +0200

    ADd get cursor script

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ gcc prog-1.cc $(pkg-config --cflags --libs x11) -o prog-1
 gcc prog-2.cc $(pkg-config --cflags --libs x11) -o prog-2
 gcc prog-3.cc $(pkg-config --cflags --libs x11) -o prog-3
 gcc highlight.cc $(pkg-config --cflags --libs x11) -o highlight
+gcc get_cursor.cc $(pkg-config --cflags --libs x11) -o get_cursor
 ```
---
+++ get_cursor.cc
@@ -0,0 +1,29 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/Xlib.h>
+
+int main(int argc, char* argv[]) {
+    // Setup display and such
+    char *display_name = getenv("DISPLAY");
+    if (!display_name) {
+        fprintf(stderr, "%s: cannot connect to X server '%s'\n", argv[0], display_name);
+        exit(1);
+    }
+
+    Display *display = XOpenDisplay(display_name);
+    int screen = DefaultScreen(display);
+
+    // Get the mouse cursor position
+    int win_x, win_y, root_x, root_y = 0;
+    unsigned int mask = 0;
+    Window child_win, root_win;
+    XQueryPointer(display, XRootWindow(display, screen),
+        &child_win, &root_win,
+        &root_x, &root_y, &win_x, &win_y, &mask);
+
+    printf("root %u %u\n", root_x, root_y);
+
+    XCloseDisplay(display);
+}
