2ed2c16a67d337d6f0635c195b3edbf1adb19da4
Mauricio Prado <mauricioprado00@gmail.com>
2020-04-21 16:26:58 +0200

    Add line tool for ui support

---
+++ line-tool.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CONFIG_DIR=~/.config/line-tool/
+from_file=${CONFIG_DIR}"from"
+to_file=${CONFIG_DIR}"to"
+
+if [ ! -d $CONFIG_DIR ]; then
+    mkdir -p $CONFIG_DIR
+fi
+
+if [ "$1" == "from" ]; then
+    echo capturing from to $from_file
+    get_cursor > $from_file
+    highlight-point $(cat $from_file) red 100
+elif [ "$1" == "to" ]; then
+    echo capturing to to $to_file
+    get_cursor > $to_file
+    highlight-point $(cat $to_file) green 100
+elif [ "$1" == "show" ]; then
+    draw_line $(cat $from_file) $(cat $to_file) green 500
+fi
+
