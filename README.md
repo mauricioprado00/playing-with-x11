c3ce99afeb8f5efbe8aeb455d106973d2088652c
Mauricio Prado <mauricioprado00@gmail.com>
2020-04-22 20:57:17 +0200

    Add script to take a screenshot

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ gcc prog-1.cc $(pkg-config --cflags --libs x11) -o bin/prog-1
 gcc prog-2.cc $(pkg-config --cflags --libs x11) -o bin/prog-2
 gcc prog-3.cc $(pkg-config --cflags --libs x11) -o bin/prog-3
 gcc highlight.cc $(pkg-config --cflags --libs x11) -o bin/highlight
+gcc cursorshot.cc $(pkg-config --cflags --libs x11) -o bin/cursorshot
 gcc get_cursor.cc $(pkg-config --cflags --libs x11) -o bin/get_cursor
 gcc overlay.cc $(pkg-config --cflags --libs x11) -o bin/overlay
 gcc overlay2.cc $(pkg-config --cflags --libs x11) -o bin/overlay2
---
+++ cursorshot.cc
@@ -0,0 +1,111 @@
+/*
+ * Takes a screenshot on the location of the mouse cursor
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/Xcomposite.h>
+
+// Some variables you can play with :-)
+int size = 220;
+int step = 40;
+
+/**
+ * Save image to a PPM image file format
+ * @see https://en.wikipedia.org/wiki/Netpbm#File_formats
+ */
+void save_ppm(XImage *pImage) {
+    FILE *fp;
+    int i, j;
+    char *pointer;
+
+    fp = fopen("screenshot.ppm", "wb");
+
+    if(fp == NULL)
+        return;
+
+    fprintf(fp, "P3\n");
+    fprintf(fp, "%d %d\n", pImage->width, pImage->height);
+    fprintf(fp, "255\n");
+
+    pointer = pImage->data;
+    for (i = 0; i < pImage->width; i++) {
+        for (j = 0; j < pImage->height; j++) {
+            fprintf(fp, "%hhu %hhu %hhu\n", *(char *)(pointer+2), *(char *)(pointer + 1), *(char *)(pointer + 0));
+            // move to next pixel
+            pointer += 4;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // Setup display and such
+    char *display_name = getenv("DISPLAY");
+    if (!display_name) {
+        fprintf(stderr, "%s: cannot connect to X server '%s'\n", argv[0], display_name);
+        exit(1);
+    }
+
+    Display *display = XOpenDisplay(display_name);
+    int screen = DefaultScreen(display);
+
+    // Get the mouse cursor position
+    int win_x, win_y, root_x, root_y = 0;
+    unsigned int mask = 0;
+    Window child_win, root_win;
+    XQueryPointer(display, XRootWindow(display, screen),
+        &child_win, &root_win,
+        &root_x, &root_y, &win_x, &win_y, &mask);
+
+    // Create a window at the mouse position
+    XSetWindowAttributes window_attr;
+    window_attr.override_redirect = 1;
+    Window window = XCreateWindow(display, XRootWindow(display, screen),
+        root_x - size/2, root_y - size/2,   // x, y position
+        size, size,                         // width, height
+        0,                                  // border width
+        DefaultDepth(display, screen),      // depth
+        CopyFromParent,                     // class
+        DefaultVisual(display, screen),     // visual
+        CWOverrideRedirect,                 // valuemask
+        &window_attr                        // attributes
+    );
+    XMapWindow(display, window);
+    XStoreName(display, window, "find-cursor");
+
+    XClassHint *xclass = XAllocClassHint();
+    xclass->res_name = "find-cursor";
+    xclass->res_class = "find-cursor";
+    XSetClassHint(display, window, xclass);
+    XFree(xclass);
+
+    // Keep the window on top
+    XEvent e;
+    memset(&e, 0, sizeof(e));
+    e.xclient.type = ClientMessage;
+    e.xclient.message_type = XInternAtom(display, "_NET_WM_STATE", False);
+    e.xclient.display = display;
+    e.xclient.window = window;
+    e.xclient.format = 32;
+    e.xclient.data.l[0] = 1;
+    e.xclient.data.l[1] = XInternAtom(display, "_NET_WM_STATE_STAYS_ON_TOP", False);
+    XSendEvent(display, XRootWindow(display, screen), False, SubstructureRedirectMask, &e);
+
+    XRaiseWindow(display, window);
+    XFlush(display);
+
+    // get window image
+    XImage *image;
+    XWindowAttributes attr;
+    XGetWindowAttributes(display, window, &attr);
+    image = XGetImage(display,window,0,0,attr.width,attr.height,AllPlanes,ZPixmap);
+    save_ppm(image);
+
+    XCloseDisplay(display);
+}
+
