6e789c201bdf290558b9e3d5ea2ba39cad3cbe72
Mauricio Prado <mauricioprado00@gmail.com>
2020-05-02 02:40:45 +0200

    Add two commands to line tool

--- line-tool.sh
+++ line-tool.sh
@@ -3,6 +3,7 @@
 CONFIG_DIR=~/.config/line-tool/
 from_file=${CONFIG_DIR}"from"
 to_file=${CONFIG_DIR}"to"
+save_file=${CONFIG_DIR}"save"
 
 if [ ! -d $CONFIG_DIR ]; then
     mkdir -p $CONFIG_DIR
@@ -16,6 +17,14 @@ elif [ "$1" == "to" ]; then
     echo capturing to to $to_file
     get_cursor > $to_file
     highlight-point $(cat $to_file) green 100
+elif [ "$1" == "save" ]; then
+    echo saving position $save_file
+    get_cursor > $save_file
+    highlight-point $(cat $save_file) violet 100
+elif [ "$1" == "restore" ]; then
+    echo restoring position $save_file
+    xdotool mousemove $(cat $save_file)
+    highlight-point $(cat $save_file) yellow 100
 elif [ "$1" == "show" ]; then
     draw_line $(cat $from_file) $(cat $to_file) black 500
 fi
